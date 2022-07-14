77ea3af131e4e5e9fcd48c304e539477b5bafe92
Albert <ishengomaalbert@gmail.com>
2022-07-14 12:08:34 +0300

    Models & Schema using Mongoose

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const Workout = require("../models/Workout");
 const router = express.Router();
 
 //GET all workouts
@@ -7,16 +7,22 @@ router.get("/", (req, res) => {
   res.json({ msg: "GET all workouts" });
 });
 
-
 //GET single workout
 router.get("/:id", (req, res) => {
   res.json({ msg: "GET single workout" });
 });
 
-
 //POST a new workout
-router.post("/", (req, res) => {
-  res.json({ msg: "POST a new workout" });
+router.post("/", async (req, res) => {
+  const { title, reps, load } = req.body;
+
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(201).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+
 });
 
 //DELETE a single workout
@@ -29,6 +35,4 @@ router.patch("/:id", (req, res) => {
   res.json({ msg: "UPDATE a workout" });
 });
 
-
-
-module.exports = router
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,31 @@
-require('dotenv').config()
-const express = require('express');
-const workoutRoutes = require('./routes/workouts')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
 
-//Express app
+//express app
 const app = express();
 
-//midleware
+//midlewares
 app.use(express.json());
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-//Routes
-app.use('/api/workouts', workoutRoutes)
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for request
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to db & listening for request on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-//listen for request
-app.listen(process.env.PORT , () => {
-    console.log(`Listening for request on port ${process.env.PORT}`);
-})
+//routes
+app.use("/api/workouts", workoutRoutes);
